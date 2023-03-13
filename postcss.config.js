5d4ee2104a7c369875b52eab109bbbb0217f4007
Michael Bykovsky <m.a.bykovsky@gmail.com>
2023-03-14 03:31:29 +0500

    + browserlists (is working?), changed postcss config, changed styles (for testing)

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     plugins: [
-        require('precss'),
-        require('autoprefixer'),
-        require('postcss-import'),
         require('postcss-preset-env'),
-        require('cssnano')
     ]
-} 
+}
